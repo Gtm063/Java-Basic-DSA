c408755de9a2bd6755bd586ae520a0772b29b52a
Gtm063 <gtmsgtm333@gmail.com>
2026-09-25 18:59:06 +0530

    Java DSA practice code

--- src/Binary_search/MountainArray.java
+++ src/Binary_search/MountainArray.java
@@ -1,5 +1,6 @@
 package Binary_search;
 //https://leetcode.com/problems/peak-index-in-a-mountain-array/
+//https://leetcode.com/problems/find-peak-element/
 public class MountainArray {
     public static void main(String[] args) {
      int []arr={1,2,3,4,3,1};
